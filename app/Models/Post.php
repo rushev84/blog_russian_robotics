8890a0da94a263abe68482f0bfb1555c199d1223
SergeyRushev <rushevs198417@gmail.com>
2023-08-05 10:21:26 +0300

    refactor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
 
     protected $fillable = [
         'id',
-        'title', 'slug', 'description',
+        'title', 'content', 'slug', 'category_id',
     ];
 
     protected $hidden = [
