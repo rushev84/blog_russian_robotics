20a1f63bffbe649b16154d8da5f771b5f548315c
SergeyRushev <rushevs198417@gmail.com>
2023-08-09 13:20:35 +0300

    refactor

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
-Route::get('/{category_slug}/{post_slug}', [PostController::class, 'single'])->name('post.single');
+Route::get('/posts/{category_slug}/{post_slug}', [PostController::class, 'single'])->name('post.single');
 
